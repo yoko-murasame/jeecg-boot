5d27e87087662a36731d5f167f9b25b322a3cab0
Yoko <kuluseky@gmail.com>
2024-04-13 09:17:52 +0800

    [ADD]Dubbo改造-添加jeecg-demo-dubbo-start模块、调整POM&配置&日志

---
+++ jeecg-server-cloud/jeecg-demo-dubbo-start/src/main/java/org/jeecg/JeecgDemoDubboApplication.java
@@ -0,0 +1,17 @@
+package org.jeecg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableCaching
+@EnableScheduling
+public class JeecgDemoDubboApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JeecgDemoDubboApplication.class, args);
+    }
+}
