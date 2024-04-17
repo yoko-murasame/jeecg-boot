630f1ed16e75110a40b68db68e78816ffd3e6b47
Yoko <kuluseky@gmail.com>
2024-04-17 09:34:19 +0800

    [UPDATE]完善Dubbo微服务Test模块

--- jeecg-server-cloud/jeecg-system-dubbo-start/src/main/java/org/jeecg/JeecgSystemDubboApplication.java
+++ jeecg-server-cloud/jeecg-system-dubbo-start/src/main/java/org/jeecg/JeecgSystemDubboApplication.java
@@ -3,6 +3,7 @@ package org.jeecg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -10,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableCaching
 @EnableScheduling
+@EnableDiscoveryClient
 public class JeecgSystemDubboApplication {
     public static void main(String[] args) {
         SpringApplication.run(JeecgSystemDubboApplication.class, args);
--- jeecg-server-cloud/jeecg-visual/jeecg-cloud-dubbo-test/jeecg-cloud-dubbo-test-service/src/main/java/org/jeecg/TestServerApplication.java
+++ jeecg-server-cloud/jeecg-visual/jeecg-cloud-dubbo-test/jeecg-cloud-dubbo-test-service/src/main/java/org/jeecg/TestServerApplication.java
@@ -4,6 +4,7 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication(scanBasePackages = "org.jeecg")
 @EnableSwagger2
 @EnableDubbo(scanBasePackages = {"${dubbo.scan.basePackages:org.jeecg.modules.provider.rpc}"})
+@EnableDiscoveryClient
 @EnableCaching
 @EnableScheduling
 public class TestServerApplication {
