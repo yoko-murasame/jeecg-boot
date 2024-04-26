7eb7a1c81bc0ff85d5d1befa357a19069a77bce6
Yoko <kuluseky@gmail.com>
2024-04-26 10:27:00 +0800

    [add]dubbo-test模块-允许业务微服务直接跨域请求接口

--- jeecg-server-cloud/jeecg-visual/jeecg-cloud-dubbo-test/jeecg-cloud-dubbo-test-service/src/main/java/cn/com/hyit/config/SwaggerConfig.java
+++ jeecg-server-cloud/jeecg-visual/jeecg-cloud-dubbo-test/jeecg-cloud-dubbo-test-service/src/main/java/cn/com/hyit/config/SwaggerConfig.java
@@ -14,6 +14,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
@@ -184,5 +187,21 @@ public class SwaggerConfig implements WebMvcConfigurer {
         };
     }
 
+    // 需要跨域就放开
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //是否允许请求带有验证信息
+        corsConfiguration.setAllowCredentials(true);
+        // 允许访问的客户端域名
+        corsConfiguration.addAllowedOriginPattern("*");
+        // 允许服务端访问的客户端请求头
+        corsConfiguration.addAllowedHeader("*");
+        // 允许访问的方法名,GET POST等
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
 
 }
