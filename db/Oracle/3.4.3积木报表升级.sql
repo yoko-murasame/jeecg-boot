c0f14b8029379bb6ec45ce559e6a4efc230a2c06
Yoko <kuluseky@gmail.com>
2024-04-29 15:52:21 +0800

    chore: 升级积木报表 -> 1.7.4 ORACLE脚本

---
+++ db/Oracle/3.4.3积木报表升级.sql
@@ -0,0 +1,27 @@
+-- 1.5.6 升级sql --
+
+-- 将 `jimu_report_data_source` 表添加 `type` 列，并设置为可空
+ALTER TABLE jimu_report_data_source
+    ADD type VARCHAR2(10) NULL;
+-- 为 `type` 列添加注释
+COMMENT ON COLUMN jimu_report_data_source.type IS '类型(report:报表;drag:仪表盘)';
+-- 更新 `jimu_report_data_source` 表的所有行的 `type` 列为 'report'
+UPDATE jimu_report_data_source
+SET type = 'report';
+
+-- 1.6.x -> 1.7.0 升级sql --
+
+-- 将 `jimu_report_share` 表的 `report_id` 列添加唯一索引
+ALTER TABLE jimu_report_share
+    ADD CONSTRAINT uniq_report_id UNIQUE (report_id);
+-- 将 `jimu_report_share` 表添加 `share_token` 列，并设置为可空
+ALTER TABLE jimu_report_share
+    ADD share_token VARCHAR2(50) NULL;
+-- 为 `share_token` 列添加注释
+COMMENT ON COLUMN jimu_report_share.share_token IS '分享token';
+-- 为 `share_token` 列添加唯一索引
+CREATE UNIQUE INDEX uniq_jrs_share_token ON jimu_report_share (share_token);
+-- 将 `jimu_report` 表添加 `py_str` 列，可空
+ALTER TABLE jimu_report ADD py_str CLOB NULL;
+-- 为 `py_str` 列添加注释
+COMMENT ON COLUMN jimu_report.py_str IS 'py增强';
