d5013b4930be165969a0fc72c44e683d05da108c
Yoko <kuluseky@gmail.com>
2024-05-06 11:11:56 +0800

    feat: 通用文件上传功能-集成视频断点续传

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/controller/CommonController.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/controller/CommonController.java
@@ -33,6 +33,7 @@ import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
@@ -40,6 +41,7 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -293,6 +295,13 @@ public class CommonController {
                 response.addHeader("Content-Disposition", "attachment;fileName=" + new String(file.getName().getBytes(
                         StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1));
             }
+            // 处理视频流
+            String suffix=imgPath.substring(imgPath.lastIndexOf(".")+1).toLowerCase();
+            if("mp4".equals(suffix)||"avi".equals(suffix)||"flv".equals(suffix)){
+                sendVideo(request,response);
+                return;
+            }
+            // 其他情况
             inputStream = new BufferedInputStream(Files.newInputStream(Paths.get(filePath)));
             outputStream = response.getOutputStream();
             byte[] buf = new byte[1024];
@@ -321,6 +330,91 @@ public class CommonController {
                 }
             }
         }
+
+    }
+
+    public void sendVideo(HttpServletRequest request, HttpServletResponse response) throws FileNotFoundException, IOException {
+        String imgPath = extractPathFromPattern(request);
+        if(oConvertUtils.isEmpty(imgPath) || CommonConstant.STRING_NULL.equals(imgPath)){
+            return;
+        }
+        imgPath = imgPath.replace("..", "").replace("../","");
+        if (imgPath.endsWith(SymbolConstant.COMMA)) {
+            imgPath = imgPath.substring(0, imgPath.length() - 1);
+        }
+        String filePath = uploadpath + File.separator + imgPath;
+        File file = new File(filePath);
+        if(!file.exists()){
+            response.setStatus(404);
+            throw new RuntimeException("文件["+imgPath+"]不存在..");
+        }
+        String fileName=file.getName();
+        RandomAccessFile randomFile = new RandomAccessFile(file, "r");//只读模式
+        long contentLength = randomFile.length();
+        String range = request.getHeader("Range");
+        int start = 0, end = 0;
+        if(range != null && range.startsWith("bytes=")){
+            String[] values = range.split("=")[1].split("-");
+            start = Integer.parseInt(values[0]);
+            if(values.length > 1){
+                end = Integer.parseInt(values[1]);
+            }
+        }
+        int requestSize = 0;
+        if(end != 0 && end > start){
+            requestSize = end - start + 1;
+        } else {
+            requestSize = Integer.MAX_VALUE;
+        }
+
+        byte[] buffer = new byte[4096];
+        response.setContentType("video/mp4");
+        response.setHeader("Accept-Ranges", "bytes");
+        response.setHeader("ETag", fileName);
+        response.setHeader("Last-Modified", new Date().toString());
+        //第一次请求只返回content length来让客户端请求多次实际数据
+        if(range == null){
+            response.setHeader("Content-length", contentLength + "");
+        }else{
+            //以后的多次以断点续传的方式来返回视频数据
+            response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);//206
+            long requestStart = 0, requestEnd = 0;
+            String[] ranges = range.split("=");
+            if(ranges.length > 1){
+                String[] rangeDatas = ranges[1].split("-");
+                requestStart = Integer.parseInt(rangeDatas[0]);
+                if(rangeDatas.length > 1){
+                    requestEnd = Integer.parseInt(rangeDatas[1]);
+                }
+            }
+            long length = 0;
+            if(requestEnd > 0){
+                length = requestEnd - requestStart + 1;
+                response.setHeader("Content-length", "" + length);
+                response.setHeader("Content-Range", "bytes " + requestStart + "-" + requestEnd + "/" + contentLength);
+            }else{
+                length = contentLength - requestStart;
+                response.setHeader("Content-length", "" + length);
+                response.setHeader("Content-Range", "bytes "+ requestStart + "-" + (contentLength - 1) + "/" + contentLength);
+            }
+        }
+        ServletOutputStream out = response.getOutputStream();
+        int needSize = requestSize;
+        randomFile.seek(start);
+        while(needSize > 0){
+            int len = randomFile.read(buffer);
+            if(needSize < buffer.length){
+                out.write(buffer, 0, needSize);
+            } else {
+                out.write(buffer, 0, len);
+                if(len < buffer.length){
+                    break;
+                }
+            }
+            needSize -= buffer.length;
+        }
+        randomFile.close();
+        out.close();
 
     }
 
