8c73b871d09cb78906a2518a9b33494413044ffc
Yoko <kuluseky@gmail.com>
2024-08-01 09:58:13 +0800

    fix: Docker - library initialization failed - unable to allocate file descriptor table - out of memory

--- docs/DevOps/shell/docker-install.sh
+++ docs/DevOps/shell/docker-install.sh
@@ -35,11 +35,12 @@ else
     # [Service]
     echo "[Service]" >> /etc/systemd/system/docker.service
     echo "Type=notify" >> /etc/systemd/system/docker.service
+    # https://stackoverflow.com/questions/68776387/docker-library-initialization-failed-unable-to-allocate-file-descriptor-tabl
     # --graph $DOCKER_FILE_PATH 用于指定 Docker 的数据存储路径
     # -graph 参数已经在 Docker 1.13.0 版本被弃用，现在推荐使用 --data-root 参数来替代。
     # --data-root 参数的作用是指定 Docker 的工作目录，所有的 Docker 数据（包括镜像和容器）都会存储在这个目录下
     # 如果需要暴露TLS连接请加参数：-H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375
-    echo "ExecStart=/usr/bin/dockerd --data-root $DOCKER_FILE_PATH --config-file $DOCKER_FILE_PATH/daemon.json" >> /etc/systemd/system/docker.service
+    echo "ExecStart=/usr/bin/dockerd --default-ulimit nofile=65536:65536 -H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375 --data-root $DOCKER_FILE_PATH --config-file $DOCKER_FILE_PATH/daemon.json" >> /etc/systemd/system/docker.service
     echo "ExecReload=/bin/kill -s HUP \$MAINPID" >> /etc/systemd/system/docker.service
     echo "LimitNOFILE=infinity" >> /etc/systemd/system/docker.service
     echo "LimitNPROC=infinity" >> /etc/systemd/system/docker.service
