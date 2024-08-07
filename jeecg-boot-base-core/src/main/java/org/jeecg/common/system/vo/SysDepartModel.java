fbe92f6e68e601c041277bb4771b7c55af0b6849
Yoko <kuluseky@gmail.com>
2024-08-07 15:48:53 +0800

    feat: 基座-部门model添加树转换方法、树排序、树深度优先遍历方法

--- jeecg-boot-base-core/src/main/java/org/jeecg/common/system/vo/SysDepartModel.java
+++ jeecg-boot-base-core/src/main/java/org/jeecg/common/system/vo/SysDepartModel.java
@@ -1,153 +1,245 @@
 package org.jeecg.common.system.vo;
 
+import lombok.Data;
+import org.jeecg.common.constant.CommonConstant;
+import org.springframework.util.StringUtils;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
  * 部门机构model
- * @author: lvdandan
  */
+@Data
 public class SysDepartModel implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    /**ID*/
+    /**
+     * ID
+     */
     private String id;
-    /**父机构ID*/
+    /**
+     * 父机构ID
+     */
     private String parentId;
-    /**机构/部门名称*/
+    /**
+     * 机构/部门名称
+     */
     private String departName;
-    /**英文名*/
+    /**
+     * 英文名
+     */
     private String departNameEn;
-    /**缩写*/
+    /**
+     * 缩写
+     */
     private String departNameAbbr;
-    /**排序*/
+    /**
+     * 排序
+     */
     private Integer departOrder;
-    /**描述*/
+    /**
+     * 描述
+     */
     private String description;
-    /**机构类别 1组织机构，2岗位*/
+    /**
+     * 机构类别 1组织机构，2岗位
+     */
     private String orgCategory;
-    /**机构类型*/
+    /**
+     * 机构类型
+     */
     private String orgType;
-    /**机构编码*/
+    /**
+     * 机构编码
+     */
     private String orgCode;
-    /**手机号*/
+    /**
+     * 手机号
+     */
     private String mobile;
-    /**传真*/
+    /**
+     * 传真
+     */
     private String fax;
-    /**地址*/
+    /**
+     * 地址
+     */
     private String address;
-    /**备注*/
+    /**
+     * 备注
+     */
     private String memo;
+    /**
+     * 状态（1启用，0不启用）
+     */
+    private String status;
+    /**
+     * 删除状态（0，正常，1已删除）
+     */
+    private String delFlag;
 
-    public String getId() {
-        return id;
+    List<SysDepartModel> children = new ArrayList<SysDepartModel>();
+
+    /**
+     * 迭代根树
+     *
+     * @param rootTreeList 根树列表
+     * @param comparator   排序
+     * @param consumer     消费
+     * @author Yoko
+     * @since 2024/8/7 上午11:53
+     */
+    public static void iteratorRootTree(List<SysDepartModel> rootTreeList, Consumer<SysDepartModel> consumer, Comparator<SysDepartModel> comparator) {
+        if (rootTreeList == null || rootTreeList.isEmpty()) {
+            return;
+        }
+        if (comparator != null) {
+            rootTreeList.sort(comparator);
+        }
+        for (SysDepartModel item : rootTreeList) {
+            consumer.accept(item);
+            if (item.getChildren() != null && !item.getChildren().isEmpty()) {
+                iteratorRootTree(item.getChildren(), consumer, comparator);
+            }
+        }
     }
 
-    public void setId(String id) {
-        this.id = id;
+    /**
+     * 迭代根树
+     *
+     * @param rootTreeList 根树列表
+     * @param consumer     消费
+     * @author Yoko
+     * @since 2024/8/7 上午11:53
+     */
+    public static void iteratorRootTree(List<SysDepartModel> rootTreeList, Consumer<SysDepartModel> consumer) {
+        iteratorRootTree(rootTreeList, consumer, SysDepartModel::compareDepartAsc);
     }
 
-    public String getParentId() {
-        return parentId;
+    /**
+     * 获取根树
+     *
+     * @param list       完整列表
+     * @param comparator 排序
+     * @param rootPid    根节点的parentId的值，默认为空字符串
+     * @return java.util.List<org.jeecg.common.system.vo.SysDepartModel>
+     * @author Yoko
+     * @since 2024/8/7 下午2:56
+     */
+    public static List<SysDepartModel> toRootTree(List<SysDepartModel> list, Comparator<SysDepartModel> comparator, String rootPid) {
+        return toTree(list, comparator).stream().filter(item -> rootPid.equals(item.getParentId())).collect(Collectors.toList());
     }
 
-    public void setParentId(String parentId) {
-        this.parentId = parentId;
+    /**
+     * 获取根树
+     *
+     * @param list       完整列表
+     * @param comparator 排序
+     * @return java.util.List<org.jeecg.common.system.vo.SysDepartModel>
+     * @author Yoko
+     * @since 2024/8/7 下午2:56
+     */
+    public static List<SysDepartModel> toRootTree(List<SysDepartModel> list, Comparator<SysDepartModel> comparator) {
+        return toTree(list, comparator).stream().filter(item -> CommonConstant.SYS_DEPART_PARENT_ID_ROOT_FLAG.equals(item.getParentId())).collect(Collectors.toList());
     }
 
-    public String getDepartName() {
-        return departName;
+    /**
+     * 获取根树
+     *
+     * @param list 完整列表
+     * @return java.util.List<org.jeecg.common.system.vo.SysDepartModel>
+     * @author Yoko
+     * @since 2024/8/7 下午2:56
+     */
+    public static List<SysDepartModel> toRootTree(List<SysDepartModel> list) {
+        return toTree(list, SysDepartModel::compareDepartAsc).stream().filter(item -> CommonConstant.SYS_DEPART_PARENT_ID_ROOT_FLAG.equals(item.getParentId())).collect(Collectors.toList());
     }
 
-    public void setDepartName(String departName) {
-        this.departName = departName;
+    /**
+     * 获取树
+     *
+     * @param list       完整列表
+     * @param comparator 排序
+     * @return java.util.List<org.jeecg.common.system.vo.SysDepartModel>
+     * @author Yoko
+     * @since 2024/8/7 下午2:56
+     */
+    public static List<SysDepartModel> toTree(List<SysDepartModel> list, Comparator<SysDepartModel> comparator) {
+        if (list == null || list.isEmpty()) {
+            return new ArrayList<>();
+        }
+        for (SysDepartModel item : list) {
+            item.children = getChildren(item, list, comparator);
+        }
+        if (null != comparator) {
+            list.sort(comparator);
+        }
+        return list;
     }
 
-    public String getDepartNameEn() {
-        return departNameEn;
+    /**
+     * 获取树-递归
+     */
+    private static List<SysDepartModel> getChildren(SysDepartModel item, List<SysDepartModel> list, Comparator<SysDepartModel> comparator) {
+        List<SysDepartModel> children = new ArrayList<>();
+        for (SysDepartModel child : list) {
+            if (item.getId().equals(child.getParentId())) {
+                children.add(child);
+            }
+        }
+        return toTree(children, comparator);
     }
 
-    public void setDepartNameEn(String departNameEn) {
-        this.departNameEn = departNameEn;
+    /**
+     * 字符串数组ASC排序
+     *
+     * @param a
+     * @param b
+     * @return int
+     * @author Yoko
+     * @since 2024/8/7 上午10:57
+     */
+    private static int compareStrAsc(String a, String b) {
+        if (!StringUtils.hasText(a)) {
+            return -1;
+        }
+        try {
+            Integer aa = Integer.parseInt(a);
+            Integer bb = Integer.parseInt(b);
+            return aa.compareTo(bb);
+        } catch (NumberFormatException e) {
+            return a.compareTo(b);
+        }
     }
 
-    public String getDepartNameAbbr() {
-        return departNameAbbr;
+    /**
+     * 部门实体对象ASC排序
+     * 先根据parentId、再根据具体排序字段
+     *
+     * @param a
+     * @param b
+     * @return int
+     * @author Yoko
+     * @since 2024/8/7 上午10:57
+     */
+    private static int compareDepartAsc(SysDepartModel a, SysDepartModel b) {
+        // 先比较父项，再比较排序
+        if (!a.getParentId().equals(b.getParentId())) {
+            return compareStrAsc(a.getParentId(), b.getParentId());
+        }
+        try {
+            Integer aa = a.getDepartOrder();
+            Integer bb = b.getDepartOrder();
+            return aa.compareTo(bb);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
     }
 
-    public void setDepartNameAbbr(String departNameAbbr) {
-        this.departNameAbbr = departNameAbbr;
-    }
-
-    public Integer getDepartOrder() {
-        return departOrder;
-    }
-
-    public void setDepartOrder(Integer departOrder) {
-        this.departOrder = departOrder;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getOrgCategory() {
-        return orgCategory;
-    }
-
-    public void setOrgCategory(String orgCategory) {
-        this.orgCategory = orgCategory;
-    }
-
-    public String getOrgType() {
-        return orgType;
-    }
-
-    public void setOrgType(String orgType) {
-        this.orgType = orgType;
-    }
-
-    public String getOrgCode() {
-        return orgCode;
-    }
-
-    public void setOrgCode(String orgCode) {
-        this.orgCode = orgCode;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getFax() {
-        return fax;
-    }
-
-    public void setFax(String fax) {
-        this.fax = fax;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getMemo() {
-        return memo;
-    }
-
-    public void setMemo(String memo) {
-        this.memo = memo;
-    }
 }
