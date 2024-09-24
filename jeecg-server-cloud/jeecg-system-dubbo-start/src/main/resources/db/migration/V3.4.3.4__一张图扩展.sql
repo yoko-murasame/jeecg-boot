56778285d00bde168a3cc9589c3018a85c0748f1
Yoko <kuluseky@gmail.com>
2024-09-24 15:09:28 +0800

    fix: 基座-资源图层ddl问题

--- jeecg-server-cloud/jeecg-system-dubbo-start/src/main/resources/db/migration/V3.4.3.4__一张图扩展.sql
+++ jeecg-server-cloud/jeecg-system-dubbo-start/src/main/resources/db/migration/V3.4.3.4__一张图扩展.sql
@@ -297,11 +297,11 @@ alter table cb_resource_layer
 comment on column cb_resource_layer.online_condition is 'online列表条件';
 
 alter table cb_resource_layer
-    add if not exists layerShowName varchar(255);
+    add if not exists layer_show_name varchar(255);
 
-comment on column cb_resource_layer.layerShowName is '图层展示名称';
+comment on column cb_resource_layer.layer_show_name is '图层展示名称';
 
 alter table cb_resource_layer
-    add if not exists isDefaultOverlay boolean;
+    add if not exists is_default_overlay boolean;
 
-comment on column cb_resource_layer.isDefaultOverlay is '是否默认叠加';
+comment on column cb_resource_layer.is_default_overlay is '是否默认叠加';
