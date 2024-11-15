c2608bb52d9448110b34743130bfb3158b59b492
Yoko <kuluseky@gmail.com>
2024-11-15 16:52:15 +0800

    fix: 基座-online表单扩展字段：slot渲染Vue代码，调整为text类型

--- jeecg-module-system/jeecg-system-start/src/main/resources/db/migration/V3.4.3.5__系统BUG处理.sql
+++ jeecg-module-system/jeecg-system-start/src/main/resources/db/migration/V3.4.3.5__系统BUG处理.sql
@@ -1,3 +1,7 @@
 -- 日志记录时，长度可能不够，调整为text类型
 alter table sys_log
     alter column log_content type text using log_content::text;
+
+-- online表单扩展字段：slot渲染Vue代码，调整为text类型
+alter table onl_cgform_field
+    alter column scoped_slots_render_code type text using scoped_slots_render_code::text;
